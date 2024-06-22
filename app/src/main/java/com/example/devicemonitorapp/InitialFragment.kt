219e7e3d7491e2fffc9e00204ea49cb8b685cdf6
Daniela <daniellapavlenco@gmail.com>
2024-06-23 00:20:29 +0300

    initial

--- app/src/main/java/com/example/devicemonitorapp/InitialFragment.kt
+++ app/src/main/java/com/example/devicemonitorapp/InitialFragment.kt
@@ -21,9 +21,11 @@ import com.example.devicemonitorapp.databinding.FragmentInitialBinding
 
 class InitialFragment : Fragment() {
 
+    //Variabila pentru binding-ul layout-ului
     private var _binding: FragmentInitialBinding? = null
     private val binding get() = _binding!!
 
+    //Metoda pentru creare si inflatare a view-ului fragmentului
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -32,34 +34,39 @@ class InitialFragment : Fragment() {
         return binding.root
     }
 
+    //Metoda pentru configurarea view-ului dupa ce a fost creat
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Hide the toolbar during the splash screen
+        // Ascunde toolbar-ul in timpul ecranului splash
         (activity as? MainActivity)?.hideToolbar()
 
-        // Check and set the background drawable based on the dark mode setting
+        // Verifica si seteaza fundalul in functie de modul intunecat
         val uiModeManager = requireContext().getSystemService(Context.UI_MODE_SERVICE) as UiModeManager
         val isNightMode = uiModeManager.nightMode == UiModeManager.MODE_NIGHT_YES
 
         if (isNightMode) {
-            binding.root.setBackgroundResource(R.drawable.night1) // Replace with your night mode drawable
+            binding.root.setBackgroundResource(R.drawable.night1) // Inlocuieste cu drwable-ul pentru modul dark
         } else {
-            binding.root.setBackgroundResource(R.drawable.pic_one) // Replace with your day mode drawable
+            binding.root.setBackgroundResource(R.drawable.pic_one) // Inlocuieste cu drawable-ul pentru modul zi
         }
 
+        //Verifica statusul permisiunii necesare
         checkPermissionStatus()
 
+        //Aplica gradient pe textul butoanelor
         applyGradientToText(binding.languageSelection, R.color.orange, R.color.green)
         applyGradientToText(binding.faqs, R.color.green, R.color.orange)
         applyGradientToText(binding.aboutApp, R.color.orange, R.color.green)
         applyGradientToText(binding.nextButton, R.color.green, R.color.orange)
 
+        //Seteaza textul butoanelor
         binding.languageSelection.text = getString(R.string.language_selection)
         binding.faqs.text = getString(R.string.faqs)
         binding.aboutApp.text = getString(R.string.about_app)
         binding.nextButton.text = getString(R.string.next)
 
+        //Seteaza click listeners pentru butoane
         binding.languageSelection.setOnClickListener {
             findNavController().navigate(R.id.action_initialFragment_to_languageSelectionFragment)
         }
@@ -75,6 +82,7 @@ class InitialFragment : Fragment() {
         }
     }
 
+    //Aaplica gradient pe textul butoanelor
     private fun applyGradientToText(button: Button, startColorResId: Int, endColorResId: Int) {
         val paint = button.paint
         val width = paint.measureText(button.text.toString())
@@ -86,18 +94,22 @@ class InitialFragment : Fragment() {
         button.paint.shader = textShader
     }
 
+    //verifica statusul permisiunii necerare
     private fun checkPermissionStatus() {
         val requiredPermission = "android.permission.WRITE_SECURE_SETTINGS"
         val checkVal = ContextCompat.checkSelfPermission(requireContext(), requiredPermission)
         if (checkVal == PackageManager.PERMISSION_GRANTED) {
+            //Permisiunea a fost acordata
             Toast.makeText(requireContext(), "WRITE_SECURE_SETTINGS has been granted to the application. You may now continue!", Toast.LENGTH_LONG).show()
             showMainContent()
         } else {
+            //Permisiunea nu a fostt acordata
             Toast.makeText(requireContext(), "WRITE_SECURE_SETTINGS has not been granted to the application. Please grant access to continue.", Toast.LENGTH_LONG).show()
             showPermissionDialog()
         }
     }
 
+    //Afiseza un dialog pentru cererea permisiunii
     private fun showPermissionDialog() {
         AlertDialog.Builder(requireContext())
             .setTitle("Permission Required")
@@ -110,11 +122,13 @@ class InitialFragment : Fragment() {
             .show()
     }
 
+    //Metoda pentru curatarea binding-ului la dispitrugerea view-ului
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
 
+    //Afiseaza continutul principal
     private fun showMainContent() {
         binding.textView.visibility = View.VISIBLE
         binding.aboutAppCard.visibility = View.VISIBLE
